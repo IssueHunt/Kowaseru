62f4b389e07c227e834961bc50c0ee6f27f16bc0
Junyoung Choi <fluke8259@gmail.com>
2023-12-23 21:45:20 +0900

    Add unique to name and email

--- migrations/20231223072750_create_user.ts
+++ migrations/20231223072750_create_user.ts
@@ -3,8 +3,8 @@ import type { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('users', function (table) {
     table.increments('id')
-    table.string('email', 255).notNullable()
-    table.string('name', 255).notNullable()
+    table.string('email', 255).notNullable().unique()
+    table.string('name', 255).notNullable().unique()
     table.string('password', 255).notNullable()
   })
 }
