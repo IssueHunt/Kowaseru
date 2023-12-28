4060e43a28108ec0d2ed0b075aa08ec1c64a6652
Junyoung Choi <fluke8259@gmail.com>
2023-12-28 19:46:07 +0900

    Use currentUser instead of session.uid to determine authenticated status

--- src/render.ts
+++ src/render.ts
@@ -2,22 +2,22 @@ import pug from 'pug'
 import path from 'path'
 import { middleware, res } from 'prismy'
 import { AsyncLocalStorage } from 'async_hooks'
-import { sessionSelector } from './session'
+import { currentUserSelector } from './selectors'
 
 const rendererLocalGlobalStorage = new AsyncLocalStorage()
 
 export function render(viewFileName: string, locals?: any) {
   const localGlobal = rendererLocalGlobalStorage.getStore()
-  console.log(localGlobal)
+
   const rendered = pug.renderFile(path.join(__dirname, `views/${viewFileName}.pug`), { ...locals, global: localGlobal })
 
   return res(rendered)
 }
 
-export const rendererGlobalStorageMiddleware = middleware([sessionSelector], next => async session => {
+export const rendererGlobalStorageMiddleware = middleware([currentUserSelector], next => async currentUser => {
   const localGlobal: { [key: string]: any } = {}
-  if (session.data?.uid != null) {
-    localGlobal.uid = session.data.uid
+  if (currentUser != null) {
+    localGlobal.currentUser = currentUser
   }
 
   return new Promise(resolve => {
--- src/selectors.ts
+++ src/selectors.ts
@@ -1,3 +1,18 @@
-import { createUrlEncodedBodySelector } from 'prismy'
+import { User } from 'knex/types/tables'
+import { AsyncSelector, createUrlEncodedBodySelector } from 'prismy'
+import { sessionSelector } from './session'
+import db from './db'
 
 export const urlEncodedBodySelector = createUrlEncodedBodySelector()
+
+export const currentUserSelector: AsyncSelector<Pick<User, 'id' | 'name'> | null> = async context => {
+  const session = await sessionSelector(context)
+
+  if (session.data?.uid == null) {
+    return null
+  }
+
+  const user = (await db('users').select('id', 'name').where('id', session.data.uid).limit(1))[0]
+
+  return user
+}
