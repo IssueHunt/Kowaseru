c43b33dd695879b8c5fa5196f76a205d7534b734
Junyoung Choi <fluke8259@gmail.com>
2023-12-31 10:26:52 +0900

    Add onDelete cascade

--- migrations/20231225164125_create_posts.ts
+++ migrations/20231225164125_create_posts.ts
@@ -7,7 +7,7 @@ export async function up(knex: Knex): Promise<void> {
     table.timestamp('created_at').defaultTo(knex.fn.now())
 
     table.integer('user_id').unsigned().notNullable()
-    table.foreign('user_id').references('id').inTable('users')
+    table.foreign('user_id').references('id').inTable('users').onDelete('cascade')
   })
 }
 
